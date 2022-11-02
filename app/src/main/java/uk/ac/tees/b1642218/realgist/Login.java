897f51d3efc54d56d4e96755b31bb0ca19d93802
Adedamola1 <adedamolafunmilayo@outlook.com>
2022-11-02 21:28:33 +0000

    splashscreen

--- app/src/main/java/uk/ac/tees/b1642218/realgist/Login.java
+++ app/src/main/java/uk/ac/tees/b1642218/realgist/Login.java
@@ -4,16 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.view.WindowManager;
 
-public class Dashboard extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
     }
 
     public void onBtnSignUp (View view){
--- app/src/main/java/uk/ac/tees/b1642218/realgist/MainActivity.java
+++ app/src/main/java/uk/ac/tees/b1642218/realgist/MainActivity.java
@@ -2,9 +2,12 @@ package uk.ac.tees.b1642218.realgist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //to Hide status
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
@@ -33,18 +37,29 @@ public class MainActivity extends AppCompatActivity {
 
         //Hooks
         image = findViewById(R.id.imgWelcome);
-        logo = findViewById(R.id.txtwelcome);
+        logo = findViewById(R.id.txtWelcome);
+        slogan = findViewById(R.id.txtSlogan);
 
         image.setAnimation(topAnim);
         logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
 
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, Dashboard.class);
-                startActivity(intent);
-                finish();
+                Intent intent = new Intent(MainActivity.this, Login.class);
+
+                //for animation
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View,String>(image, "logo_image");
+                pairs[1] = new Pair<View,String>(logo, "logo_text");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent, options.toBundle());
+
+                /*startActivity(intent);
+                finish();*/
 
 
             }
